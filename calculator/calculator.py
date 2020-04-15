af09221953af6d3c8119275354ae46222afc4683
magnumelio <manutabernerllorca@gmail.com>
2020-04-15 20:39:50 +0200

    creating a calculator begginer

---
+++ calculator/calculator.py
@@ -0,0 +1,31 @@
+import re
+
+print("\nMAGIC CALCULATOR")
+print("Type quit to exit\n")
+
+
+previous = 0
+run = True
+
+def mathOp():
+    global run
+    global previous
+    equation = ""
+
+    if previous == 0:
+        equation = input("Enter your equation: ")
+    else:
+        equation = input(str(previous))
+
+    if equation == 'quit':
+        run = False
+    else:
+        equation = re.sub('[a-zA-Z,.:()" "]', '',equation)
+        
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
+
+while run:
+    mathOp()
