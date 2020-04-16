407a35f8194a2ea18c26f4b580a9ffb6b06598b0
magnumelio <manutabernerllorca@gmail.com>
2020-04-16 21:23:27 +0200

    player can play with healing spells

--- battle/main.py
+++ battle/main.py
@@ -45,8 +45,13 @@ while running:
             continue
         
         player.reduce_mp(spell.cost)
-        enemy.take_damage(magic_dmg)
-        print(bcolors.OKBLUE + '\n' + spell.name + 'deals' , str(magic_dmg), 'points of damage' + bcolors.ENDC)
+
+        if spell.type == 'white':
+            player.heal(magic_dmg)
+            print(bcolors.OKBLUE + '\n' + spell.name + ' heals for' , str(magic_dmg), 'HP.' + bcolors.ENDC)
+        elif spell.type == 'black':
+            enemy.take_damage(magic_dmg)
+            print(bcolors.OKBLUE + '\n' + spell.name + ' deals' , str(magic_dmg), 'points of damage' + bcolors.ENDC)
 
     enemy_choice = 1
 
@@ -61,7 +66,7 @@ while running:
     print('Your HP:', bcolors.OKBLUE + str(player.get_mp()) +'/' + str(player.get_max_mp()) + bcolors.ENDC + '\n')
 
     if enemy.get_hp() == 0:
-        print(bcolors.OKGREEN + 'You win!' + bcolors.ENDC)
+        print(bcolors.OKGREEN + 'You win! :)' + bcolors.ENDC)
         running = False
     elif player.get_hp() == 0:
         print( bcolors.FAIL + 'Enemy has defeated you :(' + bcolors.ENDC)
