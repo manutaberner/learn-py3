3b39c1b737307ec9f47de1ffbcebc39b42bd070d
magnumelio <manutabernerllorca@gmail.com>
2020-04-24 20:01:06 +0200

    image search and download is working PERFECT

--- web scraper/images.py
+++ web scraper/images.py
@@ -2,22 +2,34 @@ from PIL import Image
 from bs4 import BeautifulSoup
 import requests
 from io import BytesIO
+import os
 
 def StartSearch():
     search = input('Enter search term:')
     params = {'q': search}
+    dir_name = './web scraper/' + search.replace(' ','_').lower()
+
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name)
+
     r = requests.get('https://www.bing.com/images/search', params = params)
 
     soup = BeautifulSoup(r.text, 'html.parser')
     links = soup.findAll('a',{'class': 'thumb'})
 
     for item in links:
-        img_obj = requests.get(item.attrs['href'])
-        print('Getting:', item.attrs['href'])
-        title = item.attrs['href'].split('/')[-1]
-        img = Image.open(BytesIO(img_obj.content))
+        try:
+            img_obj = requests.get(item.attrs['href'])
+            print('Getting:', item.attrs['href'])
+            title = item.attrs['href'].split('/')[-1]
+            try:
+                img = Image.open(BytesIO(img_obj.content))
 
-        img.save('./web scraper/kk/' + title, img.format)
+                img.save( dir_name +'/' + title, img.format)
+            except:
+                print('COULD NOT SAVE IMAGE')
+        except: 
+            print('Could not request Image')
 
     StartSearch()
 
