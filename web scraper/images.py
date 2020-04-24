7fddce44179cce83d90d2f1016d3cbaea070a5c3
magnumelio <manutabernerllorca@gmail.com>
2020-04-24 19:43:37 +0200

    now it's working okay

--- web scraper/images.py
+++ web scraper/images.py
@@ -3,17 +3,22 @@ from bs4 import BeautifulSoup
 import requests
 from io import BytesIO
 
-search = input('Enter search term:')
-params = {'q': search}
-r = requests.get('https://www.bing.com/images/search', params = params)
+def StartSearch():
+    search = input('Enter search term:')
+    params = {'q': search}
+    r = requests.get('https://www.bing.com/images/search', params = params)
 
-soup = BeautifulSoup(r.text, 'html.parser')
-links = soup.findAll('a',{'class': 'thumb'})
+    soup = BeautifulSoup(r.text, 'html.parser')
+    links = soup.findAll('a',{'class': 'thumb'})
 
-for item in links:
-    img_obj = requests.get(item.attrs['href'])
-    print('Getting:', item.attrs['href'])
-    title = item.attrs['href']
-    img = Image.open(BytesIO(img_obj.content))
+    for item in links:
+        img_obj = requests.get(item.attrs['href'])
+        print('Getting:', item.attrs['href'])
+        title = item.attrs['href'].split('/')[-1]
+        img = Image.open(BytesIO(img_obj.content))
 
-    img.save('./kk/' + title, img.format)
+        img.save('./web scraper/kk/' + title, img.format)
+
+    StartSearch()
+
+StartSearch()
